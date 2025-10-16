cc6d4fdd2b5f3e31f3bc3d878031fae45954a8a7
jonny2025dev <akhrorbek0704@icloud.com>
2025-10-16 17:57:31 +0900

    feat: publish mai n layout components

--- next.config.ts
+++ next.config.ts
@@ -7,3 +7,17 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// import path from "path";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+//   outputFileTracingRoot: path.join(__dirname),
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, "scss")],
+//     prependData: `@import "variables.scss";`, // Har bir SCSS faylga avtomatik qo‘shiladi
+//   },
+// };
+
+// export default nextConfig;
